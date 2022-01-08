e8c7f2b42716e9aee7e343954b5a71c190ea86d2
David Božjak <bozjak@gmail.com>
2022-01-08 20:03:42 +0100

    Initial structure + day 1

---
+++ Day1_InverseCaptcha/Program.cs
@@ -0,0 +1,21 @@
+﻿var parser = new SingleLineStringInputParser<int>(int.TryParse, str => str.ToCharArray().Select(w => w.ToString()).ToArray());
+var input = new InputProvider<int>("Input.txt", parser.GetValue).ToList();
+
+Console.WriteLine($"Part 1: {SumIfEqual(input)}");
+Console.WriteLine($"Part 2: {SumIfEqual(input, input.Count / 2)}");
+
+static int SumIfEqual(IList<int> input, int offset = 1)
+{
+    int sum = 0;
+
+    for (int i = 0; i < input.Count; i++)
+    {
+        int indexToMatch = i + offset;
+        indexToMatch = indexToMatch >= input.Count ? indexToMatch - input.Count : indexToMatch;
+
+        if (input[i] == input[indexToMatch])
+            sum += input[indexToMatch];
+    }
+
+    return sum;
+}
---
+++ SantasToolbox/EnumerableExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace SantasToolbox;
+
+public static class EnumerableExtensions
+{
+    /// <summary>
+    /// Provides all different sublists of k elements from enumerable
+    /// </summary>
+    /// <param name="k"></param>
+    /// <returns></returns>
+    public static IEnumerable<IEnumerable<T>> Combinations<T>(this IEnumerable<T> elements, int k)
+    {
+        return k == 0 ? new[] { Array.Empty<T>() } :
+          elements.SelectMany((e, i) =>
+            elements.Skip(i + 1).Combinations(k - 1).Select(c => (new[] { e }).Concat(c)));
+    }
+}
