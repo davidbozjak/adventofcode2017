801788aa60d0d56b56f8de6b7423377622fee8c8
David Božjak <bozjak@gmail.com>
2022-01-09 19:22:08 +0100

    Added day 6

---
+++ Day6_MemoryBalancer/Program.cs
@@ -0,0 +1,36 @@
+﻿var singleDigitIntParser = new SingleLineStringInputParser<int>(int.TryParse, str => str.Split('\t'));
+var dataBank = new InputProvider<int>("Input.txt", singleDigitIntParser.GetValue).ToList();
+
+var stateHash = GetDataBankStateHash(dataBank);
+Dictionary<string, int> allSeenDataBankStates = new()
+{
+    { stateHash, 0 }
+};
+
+int step = 1;
+for (; ; step++)
+{
+    int max = dataBank.Max();
+    int indexOfMax = dataBank.IndexOf(max);
+    dataBank[indexOfMax] = 0;
+
+    for (int index = GetWrappedIndex(indexOfMax + 1); max > 0; index = GetWrappedIndex(index + 1), max -= 1)
+    {
+        dataBank[index]++;
+    }
+
+    stateHash = GetDataBankStateHash(dataBank);
+    if (allSeenDataBankStates.ContainsKey(stateHash))
+        break;
+
+    allSeenDataBankStates.Add(stateHash, step);
+}
+
+Console.WriteLine($"Part 1: First duplicate hash after {step} steps");
+Console.WriteLine($"Part 2: Loop length is: {step - allSeenDataBankStates[stateHash]}");
+
+static string GetDataBankStateHash(IList<int> dataBankState) =>
+    string.Join(',', dataBankState);
+
+int GetWrappedIndex(int index) =>
+    index >= dataBank.Count ? index - dataBank.Count : index;
