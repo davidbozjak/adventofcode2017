dd531d557ece2eee8858430c8f80b6a251000738
David Božjak <bozjak@gmail.com>
2022-01-16 12:48:30 +0100

    Added part 2

--- Day10_Knots/Program.cs
+++ Day10_Knots/Program.cs
@@ -1,30 +1,63 @@
 ﻿var csvParser = new SingleLineStringInputParser<int>(int.TryParse, str => str.Split(',', StringSplitOptions.RemoveEmptyEntries));
 var lengthInputs = new InputProvider<int>("Input.txt", csvParser.GetValue).ToList();
 
-var list = Enumerable.Range(0, 256).ToList();
+var part1List = Enumerable.Range(0, 256).ToList();
+KnotList(part1List, lengthInputs, 0, 0);
+Console.WriteLine($"Part 1: {part1List[0]}x{part1List[1]} = {part1List[0] * part1List[1]}");
+
+var part2Lengths = new StreamReader("Input.txt").ReadLine().ToCharArray().Select(w => (int)w).ToList();
+part2Lengths.AddRange(new[] { 17, 31, 73, 47, 23 });
+
 int currentPosition = 0;
 int skipSize = 0;
+var part2List = Enumerable.Range(0, 256).ToList();
 
-foreach (var length in lengthInputs)
+for (int i = 0; i < 64; i++)
 {
-    for (int i = currentPosition, j = Wrap(i + length - 1, list.Count), steps = 0; steps < length / 2; steps++, i = Wrap(i + 1, list.Count), j = Wrap(j - 1, list.Count))
+    (currentPosition, skipSize) = KnotList(part2List, part2Lengths, currentPosition, skipSize);
+}
+
+var hash = CreateDenseHash(part2List);
+
+Console.WriteLine($"Part 2: {hash}");
+
+static (int currentPosition, int skipSize) KnotList<T>(IList<T> list, IEnumerable<int> lengths, int currentPosition, int skipSize)
+{
+    foreach (var length in lengths)
     {
-        var tmp = list[i];
-        list[i] = list[j];
-        list[j] = tmp;
+        for (int i = currentPosition, j = Wrap(i + length - 1), steps = 0; steps < length / 2; steps++, i = Wrap(i + 1), j = Wrap(j - 1))
+        {
+            var tmp = list[i];
+            list[i] = list[j];
+            list[j] = tmp;
+        }
+
+        currentPosition = Wrap(currentPosition + length + skipSize);
+        skipSize++;
     }
 
-    Console.WriteLine($"{string.Join(',', list)}");
+    return (currentPosition, skipSize);
 
-    currentPosition = Wrap(currentPosition + length + skipSize, list.Count);
-    skipSize++;
+    int Wrap(int input)
+    {
+        int max = list.Count;
+        while (input >= max) input -= max;
+        while (input < 0) input += max;
+        return input;
+    }
 }
 
-int Wrap(int input, int max)
+static string CreateDenseHash(IList<int> list)
 {
-    while (input >= max) input -= max;
-    while (input < 0) input += max;
-    return input;
-}
+    var denseHash = new List<string>();
 
-Console.WriteLine($"Part 1: {list[0]}x{list[1]} = {list[0] * list[1]}");
+    for (int i = 0; i < 256; i += 16)
+    {
+        var hash = list[i];
+        for (int j = 0; j < 15; j++)
+            hash ^= list[i + j + 1];
+        denseHash.Add(Convert.ToString(hash, 16).PadLeft(2, '0'));
+    }
+
+    return string.Join("", denseHash);
+}
