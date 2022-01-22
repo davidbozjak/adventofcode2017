64b1b76eb8eadb56e4b349992e17c0acd56f6521
David Božjak <bozjak@gmail.com>
2022-01-22 20:43:39 +0100

    Added day 12

---
+++ Day12_Pipes/Agent.cs
@@ -0,0 +1,57 @@
+﻿using System.Diagnostics;
+
+[DebuggerDisplay("Id: {Id}")]
+class Agent
+{
+    public int Id { get; }
+
+    private readonly List<Agent> reachableAgents = new();
+    public IReadOnlyCollection<Agent> ReachableAgents => this.reachableAgents.AsReadOnly();
+
+    public Agent(int id)
+    {
+        this.Id = id;
+    }
+
+    public void AddAgents(IEnumerable<Agent> connections)
+        => this.reachableAgents.AddRange(connections);
+
+
+    public HashSet<Agent> GetAllReachableAgents()
+    {
+        var reachable = new HashSet<Agent>();
+
+        PopulateAllReachable(reachable);
+
+        return reachable;
+    }
+
+    private void PopulateAllReachable(HashSet<Agent> reachable)
+    {
+        if (reachable.Contains(this)) return;
+        reachable.Add(this);
+
+        foreach (var agent in reachableAgents)
+        {
+            agent.PopulateAllReachable(reachable);
+        }
+    }
+
+    public bool CanReach(Agent agent)
+    {
+        return CanReach(agent, new HashSet<Agent>());
+    }
+
+    private bool CanReach(Agent agent, HashSet<Agent> visited)
+    {
+        if (visited.Contains(this)) return false;
+        visited.Add(this);
+
+        if (agent == this) return true;
+
+        foreach (var reachableAgent in this.ReachableAgents)
+            if (reachableAgent.CanReach(agent, visited)) return true;
+
+        return false;
+    }
+}
---
+++ Day12_Pipes/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Text.RegularExpressions;
+
+var factory = new UniqueFactory<int, Agent>(id => new Agent(id));
+var agents = new InputProvider<Agent?>("Input.txt", GetAgent).Where(w => w != null).Cast<Agent>().ToList();
+
+var programToFind = factory.GetOrCreateInstance(0);
+Console.WriteLine($"Part 1: {agents.Where(w => w.CanReach(programToFind)).Count()}");
+
+var unsorted = agents.ToList();
+var groups = new List<HashSet<Agent>>();
+
+while (unsorted.Count > 0)
+{
+    var agent = unsorted.First();
+    unsorted.Remove(agent);
+
+    var reachable = agent.GetAllReachableAgents();
+    unsorted.RemoveAll(reachable.Contains);
+
+    groups.Add(reachable);
+}
+
+Console.WriteLine($"Part 2: {groups.Count}");
+
+bool GetAgent(string? input, out Agent? value)
+{
+    value = null;
+
+    if (input == null) return false;
+
+    Regex numRegex = new(@"\d+");
+
+    var numbers = numRegex.Matches(input).Select(w => int.Parse(w.Value)).ToList();
+
+    value = factory.GetOrCreateInstance(numbers[0]);
+    value.AddAgents(numbers.Skip(1).Select(w => factory.GetOrCreateInstance(w)));
+
+    return true;
+}
