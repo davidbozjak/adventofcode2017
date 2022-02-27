8c0827ea2c788c5178556293fdfbf08f1180a1fa
David Božjak <bozjak@gmail.com>
2022-02-27 18:09:58 +0100

    Added day 18

---
+++ Day18_Duet/Program.cs
@@ -0,0 +1,50 @@
+﻿var instructionStrings = new InputProvider<string?>("Input.txt", GetString).Cast<string>().ToList();
+
+long playedFrequency = 0;
+Func<long, long?> listenOrTerminate = value =>
+{
+    if (value != 0)
+    {
+        return null;
+    }
+    return playedFrequency;
+};
+
+var program = new SingingProgram(instructionStrings, frq => playedFrequency = frq, listenOrTerminate);
+program.Run();
+
+Console.WriteLine($"Part 1: First NonZero frequency: {playedFrequency}");
+
+var program0Queue = new Queue<long>();
+var program1Queue = new Queue<long>();
+
+int program0SendCount = 0;
+int program1SendCount = 0;
+
+var program0 = new SingingProgram(instructionStrings, frq => { program1Queue.Enqueue(frq); program0SendCount++; }, value => program0Queue.Count > 0 ? program0Queue.Dequeue() : null);
+program0.SetRegisterValue("p", 0);
+
+var program1 = new SingingProgram(instructionStrings, frq => { program0Queue.Enqueue(frq); program1SendCount++; }, value => program1Queue.Count > 0 ? program1Queue.Dequeue() : null);
+program1.SetRegisterValue("p", 1);
+
+bool ranAtleastOne;
+do
+{
+    ranAtleastOne = false;
+    ranAtleastOne |= program0.Run();
+    ranAtleastOne |= program1.Run();
+} while (ranAtleastOne);
+
+Console.WriteLine($"Part 2: {program1SendCount}");
+
+static bool GetString(string? input, out string? value)
+{
+    value = null;
+
+    if (input == null) return false;
+
+    value = input ?? string.Empty;
+
+    return true;
+}
+
---
+++ Day18_Duet/SingingProgram.cs
@@ -0,0 +1,128 @@
+﻿using System.Diagnostics;
+
+class SingingProgram
+{
+    private readonly UniqueFactory<string, Register> Registers;
+    private readonly List<string> InstructionStrings;
+    private readonly Action<long> SingAction;
+    private readonly Func<long, long?> ListenFunc;
+    private int instructionIndex;
+
+    public SingingProgram(IEnumerable<string> instructions, Action<long> singAction, Func<long, long?> listenFunc)
+    {
+        this.InstructionStrings = instructions.ToList();
+        this.SingAction = singAction;
+        this.ListenFunc = listenFunc;
+        this.Registers = new UniqueFactory<string, Register>(name => new Register(name));
+        this.instructionIndex = 0;
+    }
+
+    public void SetRegisterValue(string register, long value)
+    {
+        var r = Registers.GetOrCreateInstance(register);
+        r.Value = value;
+    }
+
+    public bool Run()
+    {
+        bool ranAtLeastOneCommand = false;
+        for (; instructionIndex >= 0 && instructionIndex < InstructionStrings.Count; instructionIndex++)
+        {
+            var instructionString = InstructionStrings[instructionIndex];
+
+            var parts = instructionString.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            var instruction = parts[0];
+
+            if (instruction == "snd")
+            {
+                var value = GetValueOrRegisterValue(parts[1]);
+
+                this.SingAction(value);
+            }
+            else if (instruction == "set")
+            {
+                var r = Registers.GetOrCreateInstance(parts[1]);
+
+                var value = GetValueOrRegisterValue(parts[2]);
+
+                r.Value = value;
+            }
+            else if (instruction == "add")
+            {
+                var r = Registers.GetOrCreateInstance(parts[1]);
+
+                var value = GetValueOrRegisterValue(parts[2]);
+
+                r.Value += value;
+            }
+            else if (instruction == "mul")
+            {
+                var r = Registers.GetOrCreateInstance(parts[1]);
+
+                var value = GetValueOrRegisterValue(parts[2]);
+
+                r.Value *= value;
+            }
+            else if (instruction == "mod")
+            {
+                var r = Registers.GetOrCreateInstance(parts[1]);
+
+                var value = GetValueOrRegisterValue(parts[2]);
+
+                r.Value %= value;
+            }
+            else if (instruction == "rcv")
+            {
+                var r = Registers.GetOrCreateInstance(parts[1]);
+
+                var value = this.ListenFunc(r.Value);
+
+                if (value == null)
+                { 
+                    return ranAtLeastOneCommand;
+                }
+
+                r.Value = value.Value;
+            }
+            else if (instruction == "jgz")
+            {
+                var valueX = GetValueOrRegisterValue(parts[1]);
+
+                if (valueX > 0)
+                {
+                    var valueY = GetValueOrRegisterValue(parts[2]);
+
+                    instructionIndex += (int)(valueY - 1);
+                }
+            }
+            else throw new Exception("Unknown instruction");
+
+            ranAtLeastOneCommand = true;
+        }
+
+        return ranAtLeastOneCommand;
+
+        long GetValueOrRegisterValue(string valueOrRegister)
+        {
+            if (!long.TryParse(valueOrRegister, out long value))
+            {
+                var rx = Registers.GetOrCreateInstance(valueOrRegister);
+                value = rx.Value;
+            }
+
+            return value;
+        }
+    }
+
+    [DebuggerDisplay("{Name}:{Value}")]
+    protected class Register
+    {
+        public string Name { get; }
+        public long Value { get; set; } = 0;
+
+        public Register(string name)
+        {
+            this.Name = name;
+        }
+    }
+}
