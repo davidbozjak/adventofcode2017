f1eefc704ed460739fbe68da4c9cee2fe8748829
David Božjak <bozjak@gmail.com>
2022-04-10 11:29:33 +0200

    Added day25, hardcoded from input

---
+++ Day25_Turing/Program.cs
@@ -0,0 +1,118 @@
+﻿int currentIndex = 0;
+State state = State.A;
+Dictionary<int, int> tape = new();
+
+for (int i = 0; i < 12656374; i++)
+{
+    if (state == State.A)
+    {
+        if (CurrentValue() == 0)
+        {
+            tape[currentIndex] = 1;
+            currentIndex++;
+            state = State.B;
+        }
+        else if (CurrentValue() == 1)
+        {
+            tape[currentIndex] = 0;
+            currentIndex--;
+            state = State.C;
+        }
+        else throw new Exception("Unexpected current value");
+    }
+    else if (state == State.B)
+    {
+        if (CurrentValue() == 0)
+        {
+            tape[currentIndex] = 1;
+            currentIndex--;
+            state = State.A;
+        }
+        else if (CurrentValue() == 1)
+        {
+            tape[currentIndex] = 1;
+            currentIndex--;
+            state = State.D;
+        }
+        else throw new Exception("Unexpected current value");
+    }
+    else if (state == State.C)
+    {
+        if (CurrentValue() == 0)
+        {
+            tape[currentIndex] = 1;
+            currentIndex++;
+            state = State.D;
+        }
+        else if (CurrentValue() == 1)
+        {
+            tape[currentIndex] = 0;
+            currentIndex++;
+            state = State.C;
+        }
+        else throw new Exception("Unexpected current value");
+    }
+    else if (state == State.D)
+    {
+        if (CurrentValue() == 0)
+        {
+            tape[currentIndex] = 0;
+            currentIndex--;
+            state = State.B;
+        }
+        else if (CurrentValue() == 1)
+        {
+            tape[currentIndex] = 0;
+            currentIndex++;
+            state = State.E;
+        }
+        else throw new Exception("Unexpected current value");
+    }
+    else if (state == State.E)
+    {
+        if (CurrentValue() == 0)
+        {
+            tape[currentIndex] = 1;
+            currentIndex++;
+            state = State.C;
+        }
+        else if (CurrentValue() == 1)
+        {
+            tape[currentIndex] = 1;
+            currentIndex--;
+            state = State.F;
+        }
+        else throw new Exception("Unexpected current value");
+    }
+    else if (state == State.F)
+    {
+        if (CurrentValue() == 0)
+        {
+            tape[currentIndex] = 1;
+            currentIndex--;
+            state = State.E;
+        }
+        else if (CurrentValue() == 1)
+        {
+            tape[currentIndex] = 1;
+            currentIndex++;
+            state = State.A;
+        }
+        else throw new Exception("Unexpected current value");
+    }
+    else throw new Exception("Unexpected state");
+}
+
+Console.WriteLine($"Part 1: {tape.Values.Sum()}");
+
+int CurrentValue()
+{
+    if (!tape.ContainsKey(currentIndex))
+    {
+        tape[currentIndex] = 0;
+    }
+
+    return tape[currentIndex];
+}
+
+enum State { A, B, C, D, E, F }
